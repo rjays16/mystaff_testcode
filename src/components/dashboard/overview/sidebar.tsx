363e754bbd6e59de74d6869958f82fbfc5d59fd3
Allan <rjaycondiman81@gmail.com>
2024-03-22 10:21:19 +0800

    Push update for changing div to Box

--- src/components/dashboard/overview/sidebar.tsx
+++ src/components/dashboard/overview/sidebar.tsx
@@ -1,77 +1,257 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
-import { width } from '@mui/system';
-import styles from './styles.module.css';
 
 export default function Sidebar(): React.JSX.Element {
   return (
-    <div className={styles.frame51}>
-    <div className={styles.frame37}>
-      <div className={styles.frame40}>
-        <div className={styles.frame39}>
-          <div className={styles.frame13}>
-          </div>
-          <div className={styles.frame38}>
-            <div className={styles.chrissielee}>
-              Chrissie Lee
-            </div>
-            <span className={styles.client}>
-              Client
-            </span>
-          </div>
-        </div>
-      </div>
-      <Divider/>
-      <div className={styles.frame46}>
-        <div className={styles.contactinformation}>
-          CONTACT INFORMATION
-        </div>
-        <div className={styles.frame44}>
-          <div className={styles.frame42}>
-            <div className={styles.frame48}>
-              <div className={styles.icmessages}>
-                <img src="/assets/email.png" className={styles.vector79} />
-              </div>
-              <span className={styles.email}>
-                Email
+    <Box sx={{
+      position: 'relative',
+      marginBottom: '65px',
+      width: '400px',
+      height: '964px',
+    }}>
+      <Box sx={{
+        borderBottom: '1px solid #D8D3CC',
+        background: '#FFFFFF',
+        position: 'absolute',
+        left: '0px',
+        top: '0px',
+        right: '0px',
+        bottom: '-110px',
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '20px 0 19px 0',
+        height: '100%',
+        boxSizing: 'border-box',
+      }}>
+        <Box sx={{
+          margin: '0 40px 20px 40px',
+          display: 'flex',
+          flexDirection: 'row',
+          alignSelf: 'flex-start',
+          width: 'fit-content',
+          boxSizing: 'border-box',
+        }}>
+          <Box sx={{
+            marginRight: '71.9px',
+            display: 'flex',
+            flexDirection: 'row',
+            boxSizing: 'border-box',
+          }}>
+            <Box sx={{
+              borderRadius: '100px',
+              background: 'url("/assets/Chrissie.png") 50% / cover no-repeat',
+              marginRight: '24px',
+              width: '80px',
+              height: '80px',
+            }} />
+            <Box sx={{
+              margin: '14px 0',
+              display: 'flex',
+              flexDirection: 'column',
+              boxSizing: 'border-box',
+            }}>
+              <Box sx={{
+                marginBottom: '4px',
+                display: 'inline-block',
+                overflowWrap: 'break-word',
+                fontFamily: 'Urbanist',
+                fontWeight: 700,
+                fontSize: '24px',
+                letterSpacing: '0.5px',
+                color: '#1C1C1E',
+              }}>
+                Chrissie Lee
+              </Box>
+              <span style={{
+                alignSelf: 'flex-start',
+                overflowWrap: 'break-word',
+                fontFamily: 'Urbanist',
+                fontWeight: 500,
+                fontSize: '16px',
+                letterSpacing: '0.3px',
+                color: '#9D9C9C',
+              }}>
+                Client
               </span>
-            </div>
-            <span className={styles.chrissieleegmailcom}>
-              chrissielee@gmail.com
-            </span>
-          </div>
-          <div className={styles.frame43}>
-            <div className={styles.frame47}>
-              <div className={styles.icphone}>
-                <img src='/assets/phone.png' className={styles.vector80} />
-              </div>
-              <span className={styles.phone}>
-                Phone
+            </Box>
+          </Box>
+        </Box>
+        <Divider/>
+        <Box sx={{
+          margin: '0 40px 20px 40px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignSelf: 'flex-start',
+          width: 'fit-content',
+          boxSizing: 'border-box',
+        }}>
+          <Box sx={{
+            marginBottom: '20px',
+            display: 'inline-block',
+            alignSelf: 'flex-start',
+            overflowWrap: 'break-word',
+            fontFamily: 'Urbanist',
+            fontWeight: 800,
+            fontSize: '14px',
+            letterSpacing: '0.3px',
+            color: '#9D9C9C',
+          }}>
+            CONTACT INFORMATION
+          </Box>
+          <Box sx={{
+            marginLeft: '1.7px',
+            display: 'flex',
+            flexDirection: 'column',
+            width: 'fit-content',
+            boxSizing: 'border-box',
+          }}>
+            <Box sx={{
+              marginBottom: '21px',
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignSelf: 'flex-start',
+              width: '282.5px',
+              boxSizing: 'border-box',
+            }}>
+              <Box sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                boxSizing: 'border-box',
+              }}>
+                <Box sx={{
+                  margin: '3.3px 9.7px 2.3px 0',
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  width: '16.7px',
+                  height: '13.3px',
+                  boxSizing: 'border-box',
+                }}>
+                  <img src="/assets/email.png" />
+                </Box>
+                <span sx={{
+                  overflowWrap: 'break-word',
+                  fontFamily: 'Urbanist',
+                  fontWeight: 500,
+                  fontSize: '16px',
+                  letterSpacing: '0.3px',
+                  color: '#9D9C9C',
+                }}>
+                  Email
+                </span>
+              </Box>
+              <span sx={{
+                overflowWrap: 'break-word',
+                fontFamily: 'Urbanist',
+                fontWeight: 500,
+                fontSize: '16px',
+                letterSpacing: '0.3px',
+                color: '#1C1C1E',
+              }}>
+                chrissielee@gmail.com
               </span>
-            </div>
-            <span className={styles.numb1}>
-              +01 234 567 8910
-            </span>
-          </div>
-          <div className={styles.frame4422}>
-            <div className={styles.frame4823}>
-              <div className={styles.icpin}>
-                <img src="/assets/address.png" className={styles.vector81} />
-              </div>
-              <span className={styles.address}>
-                Address
+            </Box>
+            <Box sx={{
+              marginBottom: '21px', 
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignSelf: 'flex-start',
+              width: '238.2px',
+              boxSizing: 'border-box',
+            }}>
+              <Box sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                boxSizing: 'border-box',
+              }}>
+                <Box sx={{
+                  margin: '2.1px 10.1px 1.4px 0',
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  width: '15.5px',
+                  height: '15.5px',
+                  boxSizing: 'border-box',
+                }}>
+                  <img src='/assets/phone.png' />
+                </Box>
+                <span sx={{
+                  overflowWrap: 'break-word',
+                  fontFamily: 'Urbanist',
+                  fontWeight: 500,
+                  fontSize: '16px',
+                  letterSpacing: '0.3px',
+                  color: '#9D9C9C',
+                }}>
+                  Phone
+                </span>
+              </Box>
+              <span sx={{
+                overflowWrap: 'break-word',
+                fontFamily: 'Urbanist',
+                fontWeight: 500,
+                fontSize: '16px',
+                letterSpacing: '0.3px',
+                color: '#1C1C1E',
+              }}>
+                +01 234 567 8910
               </span>
-            </div>
-            <span className={styles.california}>
-              1st Avenue, Golden Street,<br />
-              Springville Village,<br />
-              San Diego, California
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+            </Box>
+            <Box sx={{
+              marginLeft: '1.7px',
+              display: 'flex',
+              flexDirection: 'row',
+              width: 'fit-content',
+              boxSizing: 'border-box',
+            }}>
+              <Box sx={{
+                margin: '0 30.6px 38px 0',
+                display: 'flex',
+                flexDirection: 'row',
+                boxSizing: 'border-box',
+              }}>
+                <Box sx={{
+                  margin: '1.7px 11.3px 0.7px 0',
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  width
+                  : '13.3px',
+                  height: '16.7px',
+                  boxSizing: 'border-box',
+                }}>
+                  <img src="/assets/address.png" />
+                </Box>
+                <span sx={{
+                  overflowWrap: 'break-word',
+                  fontFamily: 'Urbanist',
+                  fontWeight: 500,
+                  fontSize: '16px',
+                  letterSpacing: '0.3px',
+                  color: '#9D9C9C',
+                }}>
+                  Address
+                </span>
+              </Box>
+              <span sx={{
+                overflowWrap: 'break-word',
+                fontFamily: 'Urbanist',
+                fontWeight: 500,
+                fontSize: '16px',
+                letterSpacing: '0.3px',
+                color: '#1C1C1E',
+              }}>
+                1st Avenue, Golden Street,<br />
+                Springville Village,<br />
+                San Diego, California
+              </span>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 }
