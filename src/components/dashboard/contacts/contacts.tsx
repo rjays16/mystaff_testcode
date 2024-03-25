9a9d86301d89b89a92ad3c0f1bb09ac53eda5b95
Allan <rjaycondiman81@gmail.com>
2024-03-25 17:17:05 +0800

    Adding spaces for deployment

--- src/components/dashboard/contacts/contacts.tsx
+++ src/components/dashboard/contacts/contacts.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+
 export default function Contacts(): React.JSX.Element {
   return (
     <Box>
--- src/components/dashboard/messages/messages.tsx
+++ src/components/dashboard/messages/messages.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+
 export default function Messages(): React.JSX.Element {
   return (
     <Box>
