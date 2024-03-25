9211001cb2c255eebf019bbefc2020058bc64c45
Allan <rjaycondiman81@gmail.com>
2024-03-25 18:17:56 +0800

    Adding underscore for props

--- src/components/core/logo.tsx
+++ src/components/core/logo.tsx
@@ -18,7 +18,7 @@ export interface LogoProps {
   width?: number;
 }
 
-export function Logo({ color = 'dark', emblem, height = HEIGHT, width = WIDTH, ...props}: LogoProps): React.JSX.Element {
+export function Logo({ color = 'dark', emblem, height = HEIGHT, width = WIDTH, ..._props}: LogoProps): React.JSX.Element {
   let url: string;
 
   if (emblem) {
@@ -43,14 +43,14 @@ export function DynamicLogo({
   colorLight = 'dark',
   height = HEIGHT,
   width = WIDTH,
-  ...props
+  ..._props
 }: DynamicLogoProps): React.JSX.Element {
   const { colorScheme } = useColorScheme();
   const color = colorScheme === 'dark' ? colorDark : colorLight;
 
   return (
     <NoSsr fallback={<Box sx={{ height: `${height}px`, width: `${width}px` }} />}>
-      <Logo color={color} height={height} width={width} {...props} />
+      <Logo color={color} height={height} width={width} />
     </NoSsr>
   );
 }
