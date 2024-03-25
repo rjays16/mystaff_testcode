5791839a997a658e8ab448b0c76e38aba2fac372
Allan <rjaycondiman81@gmail.com>
2024-03-25 14:35:41 +0800

    Push update remove spaces

--- src/app/dashboard/contacts/page.tsx
+++ src/app/dashboard/contacts/page.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
 import type { Metadata } from 'next';
-
 import Stack from '@mui/material/Stack';
 import { config } from '@/config';
 import Grid from '@mui/material/Unstable_Grid2';
 import Contacts from "@/components/dashboard/contacts/contacts"
 export const metadata = { title: `Contacts | Dashboard | ${config.site.name}` } satisfies Metadata;
-
-
 export default function Page(): React.JSX.Element {
   return (
     <Stack spacing={3}>
--- src/app/dashboard/messages/page.tsx
+++ src/app/dashboard/messages/page.tsx
@@ -2,10 +2,7 @@ import * as React from 'react';
 import type { Metadata } from 'next';
 import Stack from '@mui/material/Stack';
 import Grid from '@mui/material/Unstable_Grid2';
-
 import { config } from '@/config';
-
-
 import Messages from "@/components/dashboard/messages/messages"
 export const metadata = { title: `Messages | Dashboard | ${config.site.name}` } satisfies Metadata;
 
